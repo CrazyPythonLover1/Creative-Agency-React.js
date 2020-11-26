07220d91c02b629c210f40a2372305f44e91ff1b
CrazyPythonLover1 <mainulislamfaruqi@gmail.com>
2020-11-26 19:08:10 +0600

    sass and navbar component

--- src/components/Home/Navbar/Navbar.js
+++ src/components/Home/Navbar/Navbar.js
@@ -1,36 +1,36 @@
-import React from 'react';
+import React from "react";
+import "./Navbar.scss";
 
 const Navbar = () => {
-    return (
-        <div>
-            <div class='portfolio_home__title'>
-      <div class='logo'>
-        <div class='page_portfolio'>
-          <div class='portfolio_home__header'>
-            <div class='logoMain'>
-              <img src='https://s3-us-west-2.amazonaws.com/s.cdpn.io/217233/cagencylogo.png'/>
-            </div>
-            <div class='nav'>
-              <ul>
-                <li class='active trigger'>Our work</li>
-                <li class='trigger'>Our services</li>
-                <li class='trigger'>About us</li>
-                <li class='trigger'>Contact us</li>
-              </ul>
-            </div>
-            <div class='number black'>0161 345 3464</div>
-            <div class='hamburger black trigger'>
-              <div class='hamburger_part'></div>
-              <div class='hamburger_part'></div>
-              <div class='hamburger_part'></div>
+  return (
+    <div>
+      <div class="portfolio_home__title">
+        <div class="logo">
+          <div class="page_portfolio">
+            <div class="portfolio_home__header">
+              <div class="logoMain">
+                <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/217233/cagencylogo.png" />
+              </div>
+              <div class="nav">
+                <ul>
+                  <li class="active trigger">Our work</li>
+                  <li class="trigger">Our services</li>
+                  <li class="trigger">About us</li>
+                  <li class="trigger">Contact us</li>
+                </ul>
+              </div>
+              <div class="number black">0161 345 3464</div>
+              <div class="hamburger black trigger">
+                <div class="hamburger_part"></div>
+                <div class="hamburger_part"></div>
+                <div class="hamburger_part"></div>
+              </div>
             </div>
           </div>
-          
-            </div>
         </div>
-        </div>
-        </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;

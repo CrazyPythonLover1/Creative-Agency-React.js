fb2dad60e7f9731b258804238ddecec4b67a68d3
CrazyPythonLover1 <mainulislamfaruqi@gmail.com>
2020-12-10 22:29:16 +0600

    hover in the circle

--- src/components/Home/Sliders/MenuBottom/MenuBottom.js
+++ src/components/Home/Sliders/MenuBottom/MenuBottom.js
@@ -4,11 +4,11 @@ import './MenuBottom.scss';
 
 const MenuBottom = () => {
 
-    const [menuActive, SetMenuActive] = useState(false)
+    // const [spanHover, setSpanHover] = useState(false)
     return (
         <div>
             <ul className="menu-bottom">
-                <NavLink to="/my-protein" activeClassName="selected"  > <li> My Protein <span className="list"> </span> </li> </NavLink>
+                <NavLink to="/my-protein" activeClassName="selected"  > <li   > My Protein <span className={`list `} > </span> </li> </NavLink>
                 <NavLink to="/nike-air-max" activeClassName="selected"> <li> Nike Air Max <span className="list"> </span>  </li> </NavLink>
                 <NavLink to="/apple" activeClassName="selected"> <li>  Apple Watch <span className="list"> </span>  </li> </NavLink>
                 <NavLink to="/jade-teriyaki" activeClassName="selected"> <li> Jade Teriyaki <span className="list"> </span>  </li> </NavLink>
